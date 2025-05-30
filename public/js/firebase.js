c3d90d8018bfa5ef46bd3564940f615abe114f20
Umer Farooq <umerfarooq2821035@gmail.com>
2025-05-30 17:15:32 +0500

    Set up Firebase Hosting with GitHub integration

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJTEnfN7PykSZJoW0ePv3LR1WQLeZ2zts",
+  authDomain: "portfolio-a77d3.firebaseapp.com",
+  projectId: "portfolio-a77d3",
+  storageBucket: "portfolio-a77d3.firebasestorage.app",
+  messagingSenderId: "754168681896",
+  appId: "1:754168681896:web:608aac1f2258b8a9b6799d",
+  measurementId: "G-S45HRKQLFW",
+};
+
+const app = initializeApp(firebaseConfig);
